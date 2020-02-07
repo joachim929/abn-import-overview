27ea2dbba6fecd93deced22f9d2277f82c189ff5
unknown <joachim_urquhart@hotmail.co.uk>
2020-02-07 22:29:24 +0100

    Adjusted assign category styling

--- front/src/app/import/import.module.ts
+++ front/src/app/import/import.module.ts
@@ -12,6 +12,7 @@ import {EditCategoriesComponent} from './components/edit-categories/edit-categor
 import {EntryEditComponent} from './components/entry-edit/entry-edit.component';
 import {EntryComponent} from './components/entry/entry.component';
 import {SharedModule} from '../shared/shared.module';
+import {MatButtonModule, MatCardModule} from '@angular/material';
 
 
 @NgModule({
@@ -29,7 +30,9 @@ import {SharedModule} from '../shared/shared.module';
     CommonModule,
     ImportRoutingModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class ImportModule {
