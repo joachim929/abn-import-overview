6f3cdbd2086fc3ecb4527443f5e9cd75bb8b9371
unknown <joachim_urquhart@hotmail.co.uk>
2020-05-24 21:28:08 +0200

    More styling

--- src/app/categories/components/categories-master/categories-master.component.ts
+++ src/app/categories/components/categories-master/categories-master.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {BreakpointService} from '../../../core/services/breakpoint.service';
 
 @Component({
   selector: 'app-categories-master',
   templateUrl: './categories-master.component.html',
-  styleUrls: ['./categories-master.component.scss']
+  styleUrls: ['./categories-master.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CategoriesMasterComponent implements OnInit {
 
